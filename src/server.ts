2b1e3723f5afd7c9192fe7f753f1ebaf87b26f9b
Erico dos Santos Bispo <erico.desenvolvedor@gmail.com>
2024-11-13 23:08:24 -0300

    feat: creation of test routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,33 @@
-// 29-30 COPIAR COMANDOS
-// 32
-import fastify from "fastify";
+import fastify from "fastify"; // 35
+import crypto from "node:crypto";
 import { knex } from "./database";
+
 const app = fastify();
 
-// GET, POST, PUT, PATCH, DELETE
-// http://localhost:3333/hello
-
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+  const transaction = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 5000,
+    })
+    .returning("*");
 
-  return tables;
+  return transaction;
+});
+
+app.get("/hello1", async () => {
+  const transactions = await knex("transactions")
+    .where("amount", 1000)
+    .select("*");
+
+  return transactions;
+});
+
+app.get("/hello2", async () => {
+  const transactions = await knex("transactions").select("*");
+
+  return transactions;
 });
 
 app
